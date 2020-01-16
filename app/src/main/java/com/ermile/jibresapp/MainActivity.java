5dfd96a5755af49e218e6d59e32ec6e21cce5b2b
Ali Sahraei <ali.sahraei110@gmail.com>
2020-01-16 17:47:09 +0330

    intro slider

--- app/src/main/java/com/ermile/jibresapp/MainActivity.java
+++ app/src/main/java/com/ermile/jibresapp/MainActivity.java
@@ -1,8 +1,10 @@
 package com.ermile.jibresapp;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void playAgain(View v){
+        new SliderPrefManager(this).setStartSlider(true);
+        Intent intent = new Intent(this, IntroSliderActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
