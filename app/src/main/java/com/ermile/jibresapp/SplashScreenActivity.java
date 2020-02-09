993b2e584089ae840915de64b30eefc930354b80
Ali Sahraei <ali.sahraei110@gmail.com>
2020-02-09 14:03:09 +0330

    fix check network connections

--- app/src/main/java/com/ermile/jibresapp/SplashScreenActivity.java
+++ app/src/main/java/com/ermile/jibresapp/SplashScreenActivity.java
@@ -3,7 +3,6 @@ package com.ermile.jibresapp;
 import android.content.Context;
 import android.content.Intent;
 import android.net.ConnectivityManager;
-import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.os.Handler;
 import android.widget.Toast;
@@ -11,8 +10,9 @@ import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SplashScreenActivity extends AppCompatActivity {
-private SliderPrefManager prefMan;
-private Context context;
+    private SliderPrefManager prefMan;
+    private Context context;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,38 +20,74 @@ private Context context;
         prefMan = new SliderPrefManager(this);
 
 
+        ConnectivityManager manager = (ConnectivityManager) getSystemService(CONNECTIVITY_SERVICE);
 
+        //For 3G check
+        boolean is3g = manager.getNetworkInfo(ConnectivityManager.TYPE_MOBILE)
+                .isConnectedOrConnecting();
+        //For WiFi Check
+        boolean isWifi = manager.getNetworkInfo(ConnectivityManager.TYPE_WIFI)
+                .isConnectedOrConnecting();
 
-        if (haveNetwork()){
-             Toast.makeText(this, "Network connection is available", Toast.LENGTH_SHORT).show();
-             new Handler().postDelayed(new Runnable() {
-                 @Override
-                 public void run() {
+        System.out.println(is3g + " net " + isWifi);
+
+        if (!is3g && !isWifi) {
+            Toast.makeText(this, "Network connection is not available", Toast.LENGTH_SHORT).show();
+
+        } else {
+//                " Your method what you want to do "
+            Toast.makeText(this, "Network connection is available", Toast.LENGTH_SHORT).show();
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
                     Intent intent;
-                    if (!prefMan.startSlider()){
-                        intent = new Intent(SplashScreenActivity.this , MainActivity.class);
-                    }else {
-                        intent = new Intent(SplashScreenActivity.this , IntroSliderActivity.class);
+                    if (!prefMan.startSlider()) {
+                        intent = new Intent(SplashScreenActivity.this, MainActivity.class);
+                    } else {
+                        intent = new Intent(SplashScreenActivity.this, IntroSliderActivity.class);
                     }
                     startActivity(intent);
                     finish();
-                 }
-             },3000);
-          } else if (!haveNetwork()) {
-             Toast.makeText(this, "Network connection is not available", Toast.LENGTH_SHORT).show();
-          }
-       }
-       private boolean haveNetwork(){
-          boolean have_WIFI = false;
-          boolean have_MobileData = false;
-          ConnectivityManager check = (ConnectivityManager)getSystemService(CONNECTIVITY_SERVICE);
-          NetworkInfo[] networkInfos = check.getAllNetworkInfo();
-          for(NetworkInfo info:networkInfos){
-             if (info.getTypeName().equalsIgnoreCase("WIFI"))if (info.isConnected())have_WIFI=true;
-             if (info.getTypeName().equalsIgnoreCase("MOBILE DATA"))if (info.isConnected())have_MobileData=true;
-          }
-          return have_WIFI||have_MobileData;
-       }
+                }
+            }, 3000);
+        }
+
+    }
 }
+
+
+//        if (haveNetwork()){
+//             Toast.makeText(this, "Network connection is available", Toast.LENGTH_SHORT).show();
+//             new Handler().postDelayed(new Runnable() {
+//                 @Override
+//                 public void run() {
+//                    Intent intent;
+//                    if (!prefMan.startSlider()){
+//                        intent = new Intent(SplashScreenActivity.this , MainActivity.class);
+//                    }else {
+//                        intent = new Intent(SplashScreenActivity.this , IntroSliderActivity.class);
+//                    }
+//                    startActivity(intent);
+//                    finish();
+//                 }
+//             },3000);
+//          } else if (!haveNetwork()) {
+//             Toast.makeText(this, "Network connection is not available", Toast.LENGTH_SHORT).show();
+//          }
+//       }
+
+
+
+//       private boolean haveNetwork(){
+//          boolean have_WIFI = false;
+//          boolean have_MobileData = false;
+//          ConnectivityManager check = (ConnectivityManager)getSystemService(CONNECTIVITY_SERVICE);
+//          NetworkInfo[] networkInfos = check.getAllNetworkInfo();
+//          for(NetworkInfo info:networkInfos){
+//             if (info.getTypeName().equalsIgnoreCase("WIFI"))if (info.isConnected())have_WIFI=true;
+//             if (info.getTypeName().equalsIgnoreCase("MOBILE DATA"))if (info.isConnected())have_MobileData=true;
+//          }
+//          return have_WIFI||have_MobileData;
+//       }
 
 
