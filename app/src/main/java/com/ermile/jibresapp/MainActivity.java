865562846aa7f59e93e9ffa04db744dbd06df2b0
Ali Sahraei <ali.sahraei110@gmail.com>
2020-02-15 22:35:17 +0330

    add webview and fix design

--- app/src/main/java/com/ermile/jibresapp/MainActivity.java
+++ app/src/main/java/com/ermile/jibresapp/MainActivity.java
@@ -1,17 +1,20 @@
 package com.ermile.jibresapp;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-
+WebView webView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        webView = findViewById(R.id.webveiw);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("https://jibres.ir/dashboard");
 //        SliderPrefManager prefman = new SliderPrefManager(this);
 //        if (prefman.startSlider()){
 //            Intent intent = new Intent(this, IntroSliderActivity.class);
@@ -19,10 +22,10 @@ public class MainActivity extends AppCompatActivity {
 //            finish();
 //        }
     }
-    public void playAgain(View v){
-        new SliderPrefManager(this).setStartSlider(true);
-        Intent intent = new Intent(this, IntroSliderActivity.class);
-        startActivity(intent);
-        finish();
-    }
+//    public void playAgain(View v){
+//        new SliderPrefManager(this).setStartSlider(true);
+//        Intent intent = new Intent(this, IntroSliderActivity.class);
+//        startActivity(intent);
+//        finish();
+//    }
 }
