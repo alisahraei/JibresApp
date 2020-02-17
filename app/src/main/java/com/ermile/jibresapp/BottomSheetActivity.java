b1cee47bb032fced20fec900a378e12ff4fa945a
Ali Sahraei <ali.sahraei110@gmail.com>
2020-02-17 21:06:38 +0330

    new anime for network error

---
+++ app/src/main/java/com/ermile/jibresapp/BottomSheetActivity.java
@@ -0,0 +1,13 @@
+package com.ermile.jibresapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BottomSheetActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bottom_sheet);
+    }
+}
