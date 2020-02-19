102554b813a8965e60f5c2498e016e2c7b69b2bb
Ali Sahraei <ali.sahraei110@gmail.com>
2020-02-19 20:59:27 +0330

    refresh method

--- app/src/main/java/com/ermile/jibresapp/BottomSheetActivity.java
+++ app/src/main/java/com/ermile/jibresapp/BottomSheetActivity.java
@@ -1,14 +1,43 @@
 package com.ermile.jibresapp;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
-import androidx.appcompat.app.AppCompatActivity;
+public class BottomSheetActivity extends BottomSheetFragment {
 
-public class BottomSheetActivity extends AppCompatActivity {
+
+    listenerBottomSheet mListener;
+
+    public void setListener(listenerBottomSheet listener) {
+        mListener = listener;
+    }
+
+    public BottomSheetActivity() {
+        // Required empty public constructor
+    }
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_bottom_sheet);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.activity_bottom_sheet, container, false);
+        view.findViewById(R.id.refresh).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view1) {
+                mListener.refreh();
+            }
+        });
+        return view;
+    }
+
+    public interface listenerBottomSheet{
+        void refreh();
     }
 }
