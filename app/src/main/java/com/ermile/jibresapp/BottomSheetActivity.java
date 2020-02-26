677f88fe40554acc3e4dec5cead548abb298d377
Ali Sahraei <ali.sahraei110@gmail.com>
2020-02-26 13:17:58 +0330

    Revert "Revert "change xml bottom sheet""
    This reverts commit 0f7d5809fbd0bf07f20959a537cbf11b61ebe902.

--- app/src/main/java/com/ermile/jibresapp/BottomSheetActivity.java
+++ app/src/main/java/com/ermile/jibresapp/BottomSheetActivity.java
@@ -1,43 +1,18 @@
 package com.ermile.jibresapp;
 
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
 
-public class BottomSheetActivity extends BottomSheetFragment {
+import androidx.appcompat.app.AppCompatActivity;
 
-
-    listenerBottomSheet mListener;
-
-    public void setListener(listenerBottomSheet listener) {
-        mListener = listener;
-    }
-
-    public BottomSheetActivity() {
-        // Required empty public constructor
-    }
-
+public class BottomSheetActivity extends AppCompatActivity {
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+
     }
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.activity_bottom_sheet, container, false);
-        view.findViewById(R.id.refresh).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view1) {
-                mListener.refreh();
-            }
-        });
-        return view;
-    }
 
-    public interface listenerBottomSheet{
-        void refreh();
-    }
+
 }
