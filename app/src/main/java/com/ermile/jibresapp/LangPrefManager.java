8230133fa70386e46df129a64229ba1818ebf9ff
Ali Sahraei <ali.sahraei110@gmail.com>
2020-03-19 14:58:53 +0330

    fix test class

--- app/src/main/java/com/ermile/jibresapp/LangPrefManager.java
+++ app/src/main/java/com/ermile/jibresapp/LangPrefManager.java
@@ -1,4 +1,21 @@
 package com.ermile.jibresapp;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+
 public class LangPrefManager {
+    private Context context;
+    private SharedPreferences pref;
+    private static final String PREF_NAME = "lang-pref";
+    private static final String KEY_START = "startlang";
+    public LangPrefManager(Context context){
+        this.context = context;
+        pref = context.getSharedPreferences(PREF_NAME, Context.MODE_PRIVATE);
+    }
+    public boolean startLang(){
+        return pref.getBoolean(KEY_START, true);
+    }
+    public void setStartLang(boolean start) {
+        pref.edit().putBoolean(KEY_START, start).apply();
+    }
 }
