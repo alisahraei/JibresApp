03fbd4620c0d806c3371c46c70457b85818128e9
Ali Sahraei <ali.sahraei110@gmail.com>
2020-04-07 07:48:30 +0430

    create method get

--- app/src/main/java/com/ermile/jibresapp/AppManager.java
+++ app/src/main/java/com/ermile/jibresapp/AppManager.java
@@ -1,4 +1,22 @@
 package com.ermile.jibresapp;
 
+import android.content.Context;
+
 public class AppManager {
+    public static final String appLanguage = "appLanguage";
+
+    private AppManager(Context context) {
+        super();
+    }
+
+    public static String getAppLanguage(Context context) {
+        return AppManager
+                .get(context)
+                .getUserInfo()
+                .get(AppManager.appLanguage);
+    }
+    public static AppManager get(Context context) {
+        return new AppManager(context);
+    }
+
 }
