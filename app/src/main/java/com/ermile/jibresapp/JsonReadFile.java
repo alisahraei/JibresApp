e9aa20ffd7928152fae9a737f71cab37fdd0f0f9
Ali Sahraei <ali.sahraei110@gmail.com>
2020-04-10 16:10:27 +0430

    need json for splash

--- app/src/main/java/com/ermile/jibresapp/JsonReadFile.java
+++ app/src/main/java/com/ermile/jibresapp/JsonReadFile.java
@@ -1,4 +1,17 @@
 package com.ermile.jibresapp;
 
+import android.content.Context;
+
 class JsonReadFile {
+    private static String file_intro = "/intro_default.json";
+    private static String file_splash = "/splash_default.json";
+    private static String folder_json = "json";
+
+    public static String intro(Context context) {
+        return fileUtil.ReadFileAssets(context, folder_json + file_intro);
+    }
+
+    public static String splash(Context context) {
+        return fileUtil.ReadFileAssets(context, folder_json + file_splash);
+    }
 }
