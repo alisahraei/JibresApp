6ccec4ac57a456e9088fe55b43c0231875367aa8
Ali Sahraei <ali.sahraei110@gmail.com>
2020-04-10 16:23:32 +0430

    get json

--- app/src/main/java/com/ermile/jibresapp/JsonManager.java
+++ app/src/main/java/com/ermile/jibresapp/JsonManager.java
@@ -1,8 +1,16 @@
 package com.ermile.jibresapp;
 
 import android.content.Context;
+import android.content.SharedPreferences;
+
+import java.util.HashMap;
+import java.util.Map;
 
 class JsonManager {
+    SharedPreferences sharedPreferences;
+    public static final String json_splash = "json_splash";
+    public static final String json_intro = "json_intro";
+
     public JsonManager(Context context) {
         super(context);
     }
@@ -15,5 +23,11 @@ class JsonManager {
     }
     public static JsonManager context(Context context) {
         return new JsonManager(context);
+    }
+    public Map<String, String> getJson() {
+        HashMap<String, String> hashMap = new HashMap<>();
+        hashMap.put(json_intro, sharedPreferences.getString(json_intro, JsonReadFile.intro(this)));
+        hashMap.put(json_splash, sharedPreferences.getString(json_splash, JsonReadFile.splash(this)));
+        return hashMap;
     }
 }
